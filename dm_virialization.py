b0474a95ae57d0a6876f1037b722da45d3b45faf
Claire Williams <106639195+astro-claire@users.noreply.github.com>
2024-10-28 14:07:45 -0700

    make DM virialization file

---
+++ dm_virialization.py
@@ -0,0 +1,9 @@
+import numpy as np
+import numpy as np
+import pickle
+from concatenateclass import processedFOF
+from sys import argv
+from boundedness import get_allHalos, set_up_DM
+from fof_process import dx_wrap, dist2, set_snap_directories, open_hdf5, get_headerprops
+
+
